5837c3401deba5c6a88c8f5b2960be9b07389b5d
gsersed <45358122+gsersed@users.noreply.github.com>
2019-06-22 20:07:12 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
+res.writeHead(200, {'Content-Type':'text/plain; charset=utf-8'});
+
 const http = require('http');
-
 const port=process.env.PORT || 3000
-
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/html');
+res.writeHead(200, {'Content-Type':'text/plain; charset=utf-8'});
+res.end('<h1>한글 연동확인확인 테스트테스트</h1>');
 });
-
 server.listen(port,() => {
-  console.log(`Server running at port `+port);
+console.log(`Server running at port `+port);
 });

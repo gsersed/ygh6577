5ae394b44cfa1784fff80415a1cf657820c50aa1
gsersed <45358122+gsersed@users.noreply.github.com>
2019-06-22 14:59:18 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+function encode_utf8(res) {
+  return unescape(encodeURIComponent(res));
+}
+
+function decode_utf8(res) {
+  return decodeURIComponent(escape(res));
+}
+
 const http = require('http');
 
 const port=process.env.PORT || 3000
@@ -5,7 +13,7 @@ const port=process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>aaa</h1>');
+  res.end('<h1>연동확인확인 테스트테스트</h1>');
 });
 
 server.listen(port,() => {

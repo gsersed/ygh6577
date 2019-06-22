2978c6461b15239bb179fa6c7004040dd033d9b3
gsersed <45358122+gsersed@users.noreply.github.com>
2019-06-22 19:13:13 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.writeHead(200, {'Content-Type':'text/plain; charset=utf-8'});
-  res.end('<h1>한글 연동확인확인 테스트테스트</h1>');
+  res.end('<h1>연동확인</h1>');
 });
 server.listen(port,() => {
   console.log(`Server running at port `+port);
